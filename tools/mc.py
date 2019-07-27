476833d74fdc88f8583c6856ee5ba027be3d8cab
Stephan Kleinert <stephan@ubisonic.de>
2019-07-27 20:07:14 +0200

    dungeonscript parser improvements

--- tools/mc.py
+++ tools/mc.py
@@ -11,18 +11,44 @@ class mapElement:
 def trim(lines):
     out = []
     lineNum = 0
+    isReadingMultilineString = False
+    currentMultilineString = ""
     for i in lines:
         lineNum += 1
-        commentPos = i.find(";")
-        if (commentPos >= 0):
-            i = i[:commentPos]
-        i = i.strip()
-        if (len(i) > 0):
-            out.append((lineNum, i))
+        multilineQuotePos = i.find("\"\"\"")
+        if (multilineQuotePos >= 0):
+            if isReadingMultilineString:
+                #end reading multiline string
+                i = i.replace("\n","")  # remove last newline
+                currentMultilineString += i
+                currentMultilineString = currentMultilineString.replace("\\n","&&nl&&")
+                currentMultilineString = currentMultilineString.replace("\"\"\"","\"")
+                currentMultilineString = currentMultilineString.replace("\n","&&nl&&")
+                out.append((lineNum,currentMultilineString))
+                isReadingMultilineString = False
+                # print ("====>"+currentMultilineString+"<====")
+                continue
+            else:
+                #start reading multiline string
+                currentMultilineString = ""
+                i = i.replace("\n","")  # remove first newline
+                isReadingMultilineString = True
+        
+        if isReadingMultilineString:
+            currentMultilineString += i
+        else:
+            commentPos = i.find(";")
+            if (commentPos >= 0):
+                i = i[:commentPos]
+            i = i.strip()
+            if (len(i) > 0):
+                out.append((lineNum, i))
+
     return out
 
 def scanLabels(scrLines):
     print("Scanning labels")
+    currentLabels = []
     for lineTupel in srcLines:
         lineNum = lineTupel[0]
         line = lineTupel[1]
@@ -31,8 +57,12 @@ def scanLabels(scrLines):
                 print ("error: duplicate label definition at line",lineNum)
                 print ("       (original definition was at line "+str(gLabels.get(line))+")")
                 exit(-1)
-            gLabels[line] = lineNum
-            print (line)
+            currentLabels.append(line)
+        else:
+            for i in currentLabels:
+                gLabels[i] = lineNum
+            currentLabels=[]
+    print(gLabels)
 
 def parseScript(srcLines):
 
@@ -51,7 +81,7 @@ def parseScript(srcLines):
 
     p_boolean_literal = p_TRUE | p_FALSE
 
-    value = p_numeric_value | p_quoted_string | p_boolean_literal | pp.Word(pp.alphas)
+    value = p_numeric_value | p_quoted_string | p_boolean_literal | pp.Word(pp.alphanums)
 
     p_query = p_keywords("command")+pp.ZeroOrMore(pp.delimitedList(value))
 
@@ -86,6 +116,8 @@ srcFilename = sys.argv[1]
 print("Reading "+sys.argv[1])
 infile = open(srcFilename, "r")
 srcLines = trim(infile.readlines())
+for i in srcLines:
+    print (i)
 
 scanLabels(srcLines)
 parseScript(srcLines)
