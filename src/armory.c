b6c0ba98db11a3a1c3f78d7ef7a983791dba7f7b
Stephan Kleinert <stephan@ubisonic.de>
2020-07-30 06:41:29 +0200

    new armory UI

--- src/armory.c
+++ src/armory.c
@@ -1,16 +1,17 @@
-#include <conio.h>
 #include <c64.h>
+#include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "globals.h"
 #include "character.h"
+#include "cityUI.h"
 #include "congui.h"
+#include "globals.h"
 
 #define SHOP_INV_SIZE 32
 #define ITEMS_PER_PAGE 12
-#define RESTOCK_FREQ 5          // restock every n visits
+#define RESTOCK_FREQ 5 // restock every n visits
 
 byte *shopInventory;
 byte numCityVisits;
@@ -18,6 +19,22 @@ byte numCityVisits;
 // clang-format off
 #pragma code-name(push, "OVERLAY2");    // "CITY" segment
 // clang-format on
+
+void setupArmoryScreen() {
+    setupCityScreen();
+    revers(1);
+    textcolor(COLOR_BROWN);
+    cg_center(gSecondaryAreaLeftX, gStatusAreaTopY + 2, gSecondaryAreaWidth,
+              gCities[gCurrentCityIndex]);
+    cg_center(gSecondaryAreaLeftX, gStatusAreaTopY + 3, gSecondaryAreaWidth,
+              "armory");
+}
+
+item *itemAtRow(byte row) {
+    byte shopIdx;
+    shopIdx= row % SHOP_INV_SIZE;
+    return inventoryItemForID(shopInventory[shopIdx]);
+}
 
 byte addItemIDToShopInventory(byte itemID) {
     byte i;
@@ -143,15 +160,15 @@ void sellItem(character *shopper) {
         } while (val != 'y' && val != 'n');
         cputc(val);
         cursor(0);
-        if (val!='y') {
+        if (val != 'y') {
             return;
         }
         if (addItemIDToShopInventory(anItem->id)) {
-            shopper->inventory[slot] = 0;
+            shopper->inventory[slot]= 0;
             cursor(1);
             cputs("\r\nSell another (y/n)? ");
-            val=cg_getkey();
-            sellQuit = (val=='n');
+            val= cg_getkey();
+            sellQuit= (val == 'n');
         } else {
             puts("\nshop is full!\n--key--");
             cg_getkey();
@@ -160,27 +177,100 @@ void sellItem(character *shopper) {
     } while (!sellQuit);
 }
 
+byte chooseShopItem() {
+
+    signed char offset, shopIndex;
+    byte y, row, choice;
+    byte cmd;
+
+    offset= -5;
+    choice= 1;
+
+    textcolor(COLOR_ORANGE);
+    cputsxy(0, gMainAreaTopY, "Name        Stat  Class  Twn#");
+    textcolor(COLOR_GRAY1);
+
+    do {
+        for (row= 0; row < 17; ++row) {
+            y= gMainAreaTopY + 1 + row;
+            shopIndex= row + offset + choice - 1;
+            cg_line(gMainAreaTopY + 1 + row, 0, gMainAreaRightX, 160,
+                    COLOR_GRAY1);
+            if (shopIndex == -1) {
+                cputsxy(0, y, "(exit)");
+            }
+            if (itemAtRow(shopIndex)) {
+                if (shopIndex >= 0) {
+                    cputsxy(0, y, itemAtRow(shopIndex)->name);
+                    gotoxy(12, y);
+                    cprintf("%d", itemAtRow(shopIndex)->price);
+                }
+            } else {
+                cputsxy(0, y, "--empty--");
+            }
+        }
+
+        cg_line(gMainAreaTopY + 1 + 5, 0, gMainAreaRightX, 0, 1);
+
+        while (!kbhit()) {
+            cg_stepColor();
+        }
+        cmd= cgetc();
+
+        switch (cmd) {
+        case 17: // down
+            if (choice < GUILDSIZE)
+                choice++;
+
+            break;
+        case 145: // up
+            if (choice > 0)
+                choice--;
+
+        default:
+            break;
+        }
+    } while (cmd != 13);
+
+    cg_line(gMainAreaTopY + 1 + 5, 0, gMainAreaRightX, 0, COLOR_GRAY1);
+
+    return choice;
+}
+
 void doArmory(void) {
     char cmd;
     character *shopper;
-    cursor(1);
-    cputsxy(0, 21, "Who wants to go shopping? ");
-    cmd= cgetc();
-    if (cmd < '1' || cmd > '6') {
-        return;
-    }
-    cmd-= '1';
-    if (party[cmd] == NULL) {
+
+    setupArmoryScreen();
+    clearPartyArea();
+    textcolor(COLOR_GRAY2);
+    revers(1);
+    showCurrentParty(false, false);
+    clearMenuArea();
+    cputsxy(0, partyMemberCount(), "(leave armory)");
+    textcolor(COLOR_CYAN);
+    cg_center(gSecondaryAreaLeftX, gMenuAreaTopY + 1, gSecondaryAreaWidth,
+              "who wants");
+    cg_center(gSecondaryAreaLeftX, gMenuAreaTopY + 2, gSecondaryAreaWidth,
+              "  to go  ");
+    cg_center(gSecondaryAreaLeftX, gMenuAreaTopY + 3, gSecondaryAreaWidth,
+              "shopping?");
+
+    cmd= cg_verticalChooser(0, 0, 1, 14, partyMemberCount() + 1);
+
+    if (cmd == partyMemberCount()) {
         return;
     }
 
+    clearMenuArea();
+
     shopper= party[cmd];
+    chooseShopItem();
 
     do {
         cursor(0);
         sprintf(drbuf, "%s Armory", gCities[gCurrentCityIndex]);
-        cg_titlec(COLOR_BLUE, COLOR_GREEN, 0,
-                  drbuf);
+        cg_titlec(COLOR_BLUE, COLOR_GREEN, 0, drbuf);
         gotoxy(0, 19);
         printf("%s coins: %d", shopper->name, shopper->gold);
         puts("\n\nA)-L) buy item  S)ell item  eX)it shop");
