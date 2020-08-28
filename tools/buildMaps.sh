7e5e80de9ab918a42fa75e25610a6fb15b38a8a3
Stephan Kleinert <stephan@ubisonic.de>
2020-08-28 10:27:01 +0200

    introduces terror shack

--- tools/buildMaps.sh
+++ tools/buildMaps.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-python3 tools/mc.py mapsrc/library.drs mapdata/map000
-python3 tools/mc.py mapsrc/outdoor33.drs mapdata/out033
+python3 tools/mc.py mapsrc/library.drs      mapdata/map000
+python3 tools/mc.py mapsrc/terrorShack.drs  mapdata/map001
+python3 tools/mc.py mapsrc/outdoor33.drs    mapdata/out033
