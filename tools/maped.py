2736fcb5f9a6307891a26eeaee8545f470f88c5d
Stephan Kleinert <stephan@ubisonic.de>
2021-05-30 18:05:59 +0200

    map editor now supports resizing

--- tools/maped.py
+++ tools/maped.py
@@ -6,42 +6,52 @@ import curses.textpad
 import collections
 import os
 import pickle
+import sys
 
 from copy import deepcopy
 
+
 class mapElement:
     pass
+
 
 class mapEditor():
 
     kMapWinWidth = 20
     kMapWinHeight = 12
+    mapWinWidth = 70
+    mapWinHeight = 20
     kLowerTop = kMapWinHeight+4
     kScrollMargin = 2
 
     kDisplayCharacters = [
-                        # ---------- dungeon tiles ---------------
-                          ['.', 'D space/floor'],               #  0 : space/floor
-                          [u"\u25c6", 'D item'],                #  1 : item = diamond
-                          [u"\u007C", 'D vertical door'],       #  2 : vertical line (door)
-                          [u"\u2015", 'D horizontal door'],     #  3 : horizontal line (door)
-                          [u"\u2588", 'D wall'],                #  4 : wall = solid block
-                        # ------------ outdoor tiles -------------
-                          [',','O grass'],                      #  5 : grass
-                          ['%','O sand'],                       #  6 : sand
-                          ['#','O stone path'],                 #  7 : stone path
-                          ['t','O small trees'],                #  8 : small trees
-                          ['T','O large trees'],                #  9 : large trees
-                          ['w','O small water'],                # 10 : small water
-                          ['W','O large water'],                # 11 : lg water
-                          ['^','O hills'],                      # 12 : sm mountain
-                          ['M','O mountains'],                  # 13 : lg mountain
-                          ['c','O village'],                    # 14 : village
-                          ['C','O castle'],                     # 15 : castle
-                          ['i','O inn'],                        # 16 : inn
-                          ['d','O dungeon'],                    # 17 : dungeon
-                          ['b','O bridge']                      # 18 : bridge
-                          ]
+        # ---------- dungeon tiles ---------------
+        ['.', 'D space/floor'],  # 0 : space/floor
+        [u"\u25c6", 'D item'],  # 1 : item = diamond
+        # 2 : vertical line (door)
+        [u"\u007C", 'D vertical door'],
+        # 3 : horizontal line (door)
+        [u"\u2015", 'D horizontal door'],
+        [u"\u2588", 'D wall'],  # 4 : wall = solid block
+        # ------------ outdoor tiles -------------
+        [',', 'O grass'],  # 5 : grass
+        ['%', 'O sand'],  # 6 : sand
+                          ['#', 'O stone path'],  # 7 : stone path
+                          ['t', 'O small trees'],  # 8 : small trees
+                          ['T', 'O large trees'],  # 9 : large trees
+        # 10 : small water
+                          ['w', 'O small water'],
+                          ['W', 'O large water'],                # 11 : lg water
+        # 12 : sm mountain
+                          ['^', 'O hills'],
+        # 13 : lg mountain
+                          ['M', 'O mountains'],
+                          ['c', 'O village'],                    # 14 : village
+                          ['C', 'O castle'],                     # 15 : castle
+                          ['i', 'O inn'],                        # 16 : inn
+                          ['d', 'O dungeon'],                    # 17 : dungeon
+                          ['b', 'O bridge']                      # 18 : bridge
+    ]
 
     def setupEmptyMap(self):
 
@@ -49,7 +59,6 @@ class mapEditor():
         self.feels = [""]
         self.routines = [[0, 0, 0, 0, 0, 0, 0, 0]]
         self.copyMapElement = 0
-        self.currentFilename = ""
 
         for y in range(self.mapWidth):
             self.map.append([])
@@ -65,16 +74,6 @@ class mapEditor():
 
         self.stdscr = outwin
 
-        self.mapwin = self.stdscr.subwin(
-            self.kMapWinHeight+2, self.kMapWinWidth+2, 2, 1)
-
-        self.helpwin = self.stdscr.subwin(
-            18, 50, 3, self.kMapWinWidth+4)
-
-        self.opWin = self.stdscr.subwin(19, 80, 2, 0)
-
-        self.mapwin.keypad(True)
-
         self.cursorX = 0
         self.cursorY = 0
         self.originX = 0
@@ -84,27 +83,74 @@ class mapEditor():
 
         self.mapWidth = 32
         self.mapHeight = 32
+        self.currentFilename = sys.argv[1]
+
 
         curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
         curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)
 
         # init map structure
-
         self.setupEmptyMap()
+
+    def resizeWindows(self):
+
+        height,width = self.stdscr.getmaxyx()
+        self.rawMapWinHeight = height-4
+        self.rawMapWinWidth = width
+
+        if self.rawMapWinHeight > self.mapHeight+2:
+            self.rawMapWinHeight = self.mapHeight+2
+
+        if self.rawMapWinWidth > self.mapWidth+2:
+            self.rawMapWinWidth = self.mapWidth+2
+
+        self.kMapWinWidth = self.rawMapWinWidth-2
+        self.kMapWinHeight = self.rawMapWinHeight-2
+
+        self.mapwin = self.stdscr.subwin(
+           self.rawMapWinHeight, self.rawMapWinWidth, 1, 0)
+
+        self.topwin = self.stdscr.subwin(
+            1, width,0,0
+        )
+
+        self.bottomwin = self.stdscr.subwin(
+            3, width, self.rawMapWinHeight+1,0
+        )
+        
+
+        self.mapwin.keypad(True)
+
+
+#        self.helpwin = self.stdscr.subwin(
+#            self.kMapWinHeight+2, 0, 3, self.kMapWinWidth+4)
+
+#        except:
+#            curses.curs_set(0)
+#            self.stdscr.addstr("No room for UI. Aborting.\n"
+#                               "Please provide a screen with at least 80x24 characters.\n\n"
+#                               "- press any key -")
+#            self.stdscr.getch()
+#            exit(127)
+
 
     def refreshStatus(self):
         e = self.getCurrentMapEntry()
-        self.clearLower()
-        self.stdscr.move(0, 0)
-        self.stdscr.clrtoeol()
-        self.stdscr.addstr(0, 0, "maped 1.0 <")
-        self.stdscr.addstr(self.currentFilename)
-        self.stdscr.addstr("> ")
+        self.bottomwin.erase()
+        self.topwin.move(0, 0)
+        self.topwin.clrtoeol()
+        self.topwin.addstr(0, 0, "maped 1.0 <")
+        self.topwin.addstr(self.currentFilename)
+        self.topwin.addstr("> ")
+        self.topwin.noutrefresh()
+
         x = self.originX + self.cursorX - 1
         y = self.originY + self.cursorY - 1
-        self.stdscr.addstr(" x,y: "+str(x)+","+str(y))
-        self.stdscr.addstr(" startX,Y: "+str(self.startX)+","+str(self.startY))
-        self.stdscr.addstr(" imp: "+str(e.impassable))
+        self.bottomwin.move(0,0)
+        self.bottomwin.addstr("x,y: "+str(x)+","+str(y))
+        self.bottomwin.addstr(" startX,Y: "+str(self.startX)+","+str(self.startY))
+        self.bottomwin.addstr(" imp: "+str(e.impassable))
+        self.bottomwin.noutrefresh()
 
     def checkScrollMap(self):
         if self.cursorX > self.kMapWinWidth-self.kScrollMargin:
@@ -139,14 +185,19 @@ class mapEditor():
             for y in range(self.kMapWinHeight):
                 gX = x + self.originX
                 gY = y + self.originY
-                v = self.map[gX][gY].initiallyVisible
-                d = self.map[gX][gY].mapElementID
-                if (v == True):
-                    cp = 2
+                if gX<self.mapWidth and gY<self.mapHeight:
+                    v = self.map[gX][gY].initiallyVisible
+                    d = self.map[gX][gY].mapElementID
+                    if (v == True):
+                        cp = 2
+                    else:
+                        cp = 1
+                    self.mapwin.addstr(
+                        y+1, x+1, self.kDisplayCharacters[d][0], curses.color_pair(cp))
                 else:
-                    cp = 1
-                self.mapwin.addstr(
-                    y+1, x+1, self.kDisplayCharacters[d][0], curses.color_pair(cp))
+                    d=0
+                    cp=1
+                
 
     def getCurrentMapEntry(self):
         x = self.originX + self.cursorX - 1
@@ -154,18 +205,11 @@ class mapEditor():
         e = self.map[x][y]
         return e
 
-    def clearLower(self):
-        for i in range(5):
-            self.stdscr.move(self.kLowerTop+i, 0)
-            self.stdscr.clrtoeol()
-
-
     def getUserInput(self, prompt):
-        self.clearLower()
-        self.stdscr.addstr(self.kLowerTop, 0, prompt)
+        self.bottomwin.erase()
+        self.bottomwin.addstr(0, 0, prompt)
         curses.echo()
-        self.stdscr.refresh()
-        userInput = self.stdscr.getstr(32)
+        userInput = self.bottomwin.getstr(32)
         curses.noecho()
         return userInput
 
@@ -189,8 +233,8 @@ class mapEditor():
         self.cursorY += 1
 
     def setStartPosition(self):
-        self.startX = self.cursorX+self.originX -1
-        self.startY = self.cursorY+self.originY -1
+        self.startX = self.cursorX+self.originX - 1
+        self.startY = self.cursorY+self.originY - 1
 
     def toggleInitiallyVisible(self):
         e = self.getCurrentMapEntry()
@@ -220,39 +264,43 @@ class mapEditor():
             self.getCurrentMapEntry().initiallyVisible = self.copyMapElement.initiallyVisible
             self.getCurrentMapEntry().impassable = self.copyMapElement.impassable
             self.cursorX += 1
-    
+
     def goto(self):
-        self.clearLower()
-        self.stdscr.move(self.kLowerTop, 0)
+        self.bottomwin.erase()
+        self.bottomwin.move(0, 0)
         inCoords = self.getUserInput("goto (x,y):")
-        splitInCoords = inCoords.decode().split(',')    # python3, you SUCK!
-        self.cursorX = int(splitInCoords[0])+1
-        self.cursorY = int(splitInCoords[1])+1
-        self.clearLower()
+        try:
+            splitInCoords = inCoords.decode().split(',')    # python3, you SUCK!
+            self.cursorX = int(splitInCoords[0])+1
+            self.cursorY = int(splitInCoords[1])+1
+            self.bottomwin.clear()
+        except:
+            pass
 
     def loadMap(self):
-        loadFilename = self.getUserInput("Load file (.drm appended automatically): mapsrc/")
-        self.stdscr.addstr("\nLoading...")
-        infile = open(b"mapsrc/"+loadFilename+b".drm", "br")
-        self.currentFilename = loadFilename
+        loadFilename = self.currentFilename
+        infile = open(loadFilename, "br")
         mdata = pickle.load(infile)
         self.mapWidth = mdata["width"]
         self.mapHeight = mdata["height"]
         self.startX = mdata["startX"]
         self.startY = mdata["startY"]
         self.map = mdata["map"]
-        self.refreshMap()
+        self.resizeWindows()
         infile.close()
-        self.stdscr.addstr("\ndone.\n- press any key -")
-        self.stdscr.refresh()
-        self.stdscr.getch()
+
+    def loadOrCreateMap(self):
+        if (os.path.exists(self.currentFilename)):
+            self.loadMap()
+        else:
+            self.newMap()
 
     def _saveMap(self, fname=""):
         if (fname):
             saveFilename = fname
         else:
             saveFilename = self.getUserInput("Save file:")
-        self.currentFilename = saveFilename
+        saveFilename = self.currentFilename
         self.stdscr.addstr("\nSaving...")
         mdata = {
             "width": self.mapWidth,
@@ -261,13 +309,14 @@ class mapEditor():
             "startY": self.startY,
             "map": self.map,
         }
-        outfile = open(b"mapsrc/"+saveFilename+b".drm", "bw")
+        outfile = open(saveFilename, "bw")
         pickle.dump(mdata, outfile)
         outfile.close()
-        self.clearLower()
-        self.stdscr.addstr(self.kLowerTop, 0, "\ndone.\n- press any key -")
-        self.stdscr.refresh()
-        self.stdscr.getch()
+        self.bottomwin.clwar()
+        self.bottomwin.addstr(0, 0,
+                           "\nfile saved.\n- press any key -")
+        self.bottomwin.refresh()
+        self.bottomwin.getch()
         self.redrawStdEditorScreen()
 
     def saveMap(self):
@@ -278,7 +327,7 @@ class mapEditor():
 
     def saveMapAs(self):
         self._saveMap("")
-    
+
     def fillMap(self):
         elem = self.getCurrentMapEntry()
         for y in range(self.mapWidth):
@@ -287,7 +336,7 @@ class mapEditor():
                 self.map[y][x].mapElementID = elem.mapElementID
                 self.map[y][x].impassable = elem.impassable
                 self.map[y][x].startOpcodeIndex = elem.startOpcodeIndex
-    
+
     def trimMap(self):
         width = 0
         height = 0
@@ -301,8 +350,8 @@ class mapEditor():
             self.stdscr.addstr("new height (16-"+str(self.mapHeight)+"): ")
             height = int(self.stdscr.getstr(4))
             curses.noecho()
-   
-        newMap=[]
+
+        newMap = []
         for y in range(width):
             newRow = []
             for x in range(height):
@@ -313,25 +362,31 @@ class mapEditor():
         self.mapWidth = width
         self.redrawStdEditorScreen()
 
-    
     def newMap(self):
         width = 0
         height = 0
         while (width < 16 or width > 128 or height < 16 or height > 128):
             self.stdscr.erase()
-            self.stdscr.addstr("\n\n** new map **\n")
+            self.stdscr.addstr("\n\n** new map \"" +
+                               self.currentFilename+"\" **\n")
             self.stdscr.addstr("Width (16-128): ")
             self.stdscr.refresh()
             curses.echo()
-            width = int(self.stdscr.getstr(4))
-            self.stdscr.addstr("Height (16-128): ")
-            height = int(self.stdscr.getstr(4))
+            try:
+                width = int(self.stdscr.getstr(4))
+                self.stdscr.addstr("Height (16-128): ")
+                height = int(self.stdscr.getstr(4))
+            except:
+                curses.noecho()
+                self.stdscr.addstr("-- aborted. press any key --")
+                self.stdscr.getch()
+                exit(0)
             curses.noecho()
         self.mapHeight = height
         self.mapWidth = width
-        if (width < self.kMapWinWidth):
-            self.kMapWinWidth = width
         self.setupEmptyMap()
+        self.resizeWindows()
+
 
     def userStartup(self):
         self.stdscr.erase()
@@ -351,7 +406,7 @@ class mapEditor():
         self.stdscr.erase()
         self.mapwin.border()
         self.mapwin.addstr(0, 1, "Map")
-        self.showHelp()
+        #self.showHelp()
 
     def showHelp(self):
         self.helpwin.erase()
@@ -392,35 +447,47 @@ class mapEditor():
         self.cursorX = 1
         self.cursorY = 1
 
-        self.userStartup()
+        self.loadOrCreateMap()
         self.redrawStdEditorScreen()
 
         while 0 == stopEd:
             self.refreshMap()
             self.refreshStatus()
             self.mapwin.move(self.cursorY, self.cursorX)
-            elem = self.kDisplayCharacters[self.getCurrentMapEntry().mapElementID]
+            elem = self.kDisplayCharacters[self.getCurrentMapEntry(
+            ).mapElementID]
             elemChar = elem[0]
             elemDesc = elem[1]
-            self.stdscr.move(self.kLowerTop, 0)
-            self.stdscr.clrtoeol()
-            self.stdscr.addstr(self.kLowerTop,1,'>'+elemChar+'< '+elemDesc)
-            self.stdscr.refresh()
+            self.bottomwin.move(1, 0)
+            self.bottomwin.clrtoeol()
+            self.bottomwin.addstr(1, 0, '>'+elemChar+'< '+elemDesc)
+            self.bottomwin.noutrefresh()
+            curses.doupdate()
+
             c = self.mapwin.getch()
-            func = edcmds.get(c, 0)
-            if (func == 0):
-                func = edcmds.get(chr(c), 0)
-            if (func != 0):
-                func()
-                self.checkScrollMap()
-                self.checkBounds()
-            self.stdscr.addstr(18, 0, str(c))
-            pass
+            if c== curses.KEY_RESIZE:
+                self.resizeWindows()
+                self.redrawStdEditorScreen()
+            else:
+                func = edcmds.get(c, 0)
+                if (func == 0):
+                    func = edcmds.get(chr(c), 0)
+                if (func != 0):
+                    func()
+                    self.checkScrollMap()
+                    self.checkBounds()
+            # self.stdscr.addstr(18, 0, str(c))
+                pass
 
 
 def runMaped(aWin):
     myMaped = mapEditor(aWin)
     myMaped.runEditor()
+
+
+if len(sys.argv) != 2:
+    print("usage: "+sys.argv[0]+" <path-to-mapfile.drm>")
+    sys.exit(127)
 
 locale.setlocale(locale.LC_ALL, '')
 curses.wrapper(runMaped)
