75cd2f7399b165663bb66642faa6e1d8b02ede2d
Stephan Kleinert <stephan@ubisonic.de>
2021-06-03 16:09:59 +0200

    reoarganizing things a bit

--- tools/buildMaps.sh
+++ tools/buildMaps.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-python3 tools/mc.py mapsrc/library.drs      gamedata/map000
-python3 tools/mc.py mapsrc/terrorShack.drs  gamedata/map001
-python3 tools/mc.py mapsrc/outdoor33.drs    gamedata/out033
+python3 tools/mc.py maps/library.drs      gamedata/map000
+python3 tools/mc.py maps/terrorShack.drs  gamedata/map001
+python3 tools/mc.py maps/outdoor33.drs    gamedata/out033
